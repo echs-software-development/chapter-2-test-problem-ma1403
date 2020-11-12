7b305f71c5311570fa7cf0d859129db07d548682
ma1403 <73349536+ma1403@users.noreply.github.com>
2020-11-12 15:11:24 +0000

    I finished

--- Chapter2Test.java
+++ Chapter2Test.java
@@ -1,7 +1,14 @@
-
+import java.util.Scanner;
 
 public class Chapter2Test {
   public static void main(String[] args) {
-  
+  Scanner scan= new Scanner(System.in);
+  System.out.println("Please enter first number: ");
+  int num1=scan.nextInt();
+  System.out.println("Please enter second number: ");
+  int num2=scan.nextInt();
+  double answer=(double)(num1+num2)/2;
+  System.out.println("The average is "+ answer);
+
   }
 }
